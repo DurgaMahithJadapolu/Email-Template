fed1ab78673fa70aa69377758dc69832791bfd49
Mahith <durgamahithjadapolu@gmail.com>
2025-01-18 16:41:51 +0530

    Add missing file

--- server/index.js
+++ server/index.js
@@ -13,15 +13,12 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // MongoDB Atlas Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
-const emailConfigRoutes = require('./routes/emailConfigRoutes');
+const emailConfigRoutes = require('./Routes/emailConfigRoutes');
 app.use('/api', emailConfigRoutes);
 
 // Server health check route
