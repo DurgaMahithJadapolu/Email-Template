90b2f3649a1d1fe278899637331d83d5004efe0c
DurgaMahithJadapolu <158590615+DurgaMahithJadapolu@users.noreply.github.com>
2025-01-18 21:15:02 +0530

    Update index.js
    Router path is change

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
-const emailConfigRoutes = require('./routes/emailConfigRoutes');
+const emailConfigRoutes = require('./Routes/emailConfigRoutes');
 app.use('/api', emailConfigRoutes);
 
 
